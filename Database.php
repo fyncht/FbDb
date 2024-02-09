482c844902d4e68e952def1564eb069ba5f76d19
stanislava <43201978+fyncht@users.noreply.github.com>
2024-02-09 10:32:31 +0100

    Final solution

--- Database.php
+++ Database.php
@@ -8,6 +8,7 @@ use mysqli;
 class Database implements DatabaseInterface
 {
     private mysqli $mysqli;
+    private const SKIP_TOKEN = '/*SKIP*/'; 
 
     public function __construct(mysqli $mysqli)
     {
@@ -16,11 +17,81 @@ class Database implements DatabaseInterface
 
     public function buildQuery(string $query, array $args = []): string
     {
-        throw new Exception();
+        $offset = 0;
+        while (preg_match('/(\?d|\?f|\?a|\?\#|\{|\})/', $query, $matches, PREG_OFFSET_CAPTURE, $offset)) {
+            $match = $matches[0][0];
+            $pos = $matches[0][1];
+            $offset = $pos + strlen($match);
+
+            if ($match === '{' || $match === '}') {
+                if ($match === '{') {
+                    $endPos = strpos($query, '}', $offset);
+                    if ($endPos === false) {
+                        throw new Exception("Unmatched { in query");
+                    }
+                    $block = substr($query, $pos + 1, $endPos - $pos - 1);
+                    if (strpos($block, self::SKIP_TOKEN) !== false) {
+                        $query = substr_replace($query, '', $pos, $endPos - $pos + 1);
+                        $offset = $pos;
+                    }
+                }
+                continue;
+            }
+
+            if (!isset($args[0])) {
+                throw new Exception("Not enough parameters for query");
+            }
+
+            $value = array_shift($args);
+            switch ($match) {
+                case '?d':
+                    $replacement = intval($value);
+                    break;
+                case '?f':
+                    $replacement = floatval($value);
+                    break;
+                case '?a':
+                    if (!is_array($value)) {
+                        throw new Exception("?a specifier expects an array");
+                    }
+                    $replacement = implode(',', array_map([$this, 'escapeValue'], $value));
+                    break;
+                case '?#':
+                    $replacement = $this->escapeIdentifier($value);
+                    break;
+                default:
+                    throw new Exception("Unsupported specifier: $match");
+            }
+            $query = substr_replace($query, $replacement, $pos, strlen($match));
+            $offset = $pos + strlen($replacement);
+        }
+
+        return $query;
     }
 
     public function skip()
     {
-        throw new Exception();
+        return self::SKIP_TOKEN;
+    }
+
+    private function escapeValue($value)
+    {
+        if (is_null($value)) {
+            return 'NULL';
+        } elseif (is_bool($value)) {
+            return $value ? '1' : '0';
+        } elseif (is_int($value) || is_float($value)) {
+            return $value;
+        } else {
+            return "'" . $this->mysqli->real_escape_string($value) . "'";
+        }
+    }
+
+    private function escapeIdentifier($value)
+    {
+        if (is_array($value)) {
+            return implode(', ', array_map([$this, 'escapeIdentifier'], $value));
+        }
+        return '`' . str_replace('`', '``', $value) . '`';
     }
 }
